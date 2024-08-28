843f19f53eb4520efe45794cc93e204565f2941f
erickwork <dery.erick@outlook.com>
2024-08-28 00:41:53 -0300

    first commit

---
+++ src/main/java/br/com/flexbank/flexBankApi/FlexBankApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.flexbank.flexBankApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/flexbank/flexBankApi/FlexBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flexbank.flexBankApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
